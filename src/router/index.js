e85b612e28dc7c91aa9479e3626b834be49bff9f
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-01-22 02:20:54 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,75 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import store from '../store'
+import Home from '../views/Home.vue'
+import Login from '../views/auth/Login.vue'
+import signUp from '../views/auth/signUp.vue'
+import forgot from '../views/auth/forgot.vue'
+import forgotnew from '../views/auth/forgotPassword.vue'
+import confirmEmail from '../views/auth/confirmEmail.vue'
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'login',
+    component: Login,
+    meta: { requiresVisitor: true }
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/signUp',
+    name: 'signUp',
+    component: signUp,
+    meta: { requiresVisitor: true }
+  },
+  {
+    path: '/forgot',
+    name: 'forgot',
+    component: forgot,
+    meta: { requiresVisitor: true }
+  },
+  {
+    path: '/resetpassword/:tokenID',
+    name: 'resetpassword',
+    component: forgotnew,
+    meta: { requiresVisitor: true }
+  },
+  {
+    path: '/confirm_email/:TokenConfirm',
+    name: 'confirmEmail',
+    component: confirmEmail,
+    meta: { requiresVisitor: true }
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
-
-export default router;
+})
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.getters.isLogin) {
+      next({
+        path: '/'
+      })
+    } else {
+      next()
+    }
+  } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+    if (store.getters.isLogin) {
+      next({
+        path: '/home'
+      })
+    } else {
+      next()
+    }
+  }
+})
+export default router
