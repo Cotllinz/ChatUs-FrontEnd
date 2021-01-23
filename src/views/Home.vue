ec7e1b398d28efaa84213c04c087bd220790b79a
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-01-24 01:04:14 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <b-container>
+      <b-row>
+        <b-col style="padding: 5px" lg="4">
+          <sideProfile />
+        </b-col>
+        <b-col style="padding: 0" lg="7">
+          <chatRoom />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
+import sideProfile from '../components/_base/home/sideProfile'
+import chatRoom from '../components/_base/home/chatRoom'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    sideProfile,
+    chatRoom
+  }
 }
 </script>
+<style scoped></style>
