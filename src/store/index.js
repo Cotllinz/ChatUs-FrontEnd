849e70fe721308f6deebddf6d7ab2d8681536d57
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-01-25 04:49:20 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,9 @@ import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import Auth from './modules/auth'
 import menu from './modules/menu'
+import home from './modules/home'
+import contact from './modules/contact'
+import userAcc from './modules/user'
 Vue.use(Vuex)
 export default new Vuex.Store({
   state: {},
@@ -10,7 +13,10 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     Auth,
-    menu
+    menu,
+    home,
+    contact,
+    userAcc
   },
   plugins: [
     createPersistedState({
