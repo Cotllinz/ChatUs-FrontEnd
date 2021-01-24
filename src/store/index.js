85de691d6082fdd4b00e6d750b3cc28e62180b9a
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-01-24 08:56:40 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import Auth from './modules/auth'
+import menu from './modules/menu'
 Vue.use(Vuex)
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
   modules: {
-    Auth
+    Auth,
+    menu
   },
   plugins: [
     createPersistedState({
