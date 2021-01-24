1f7542814377d681381403487301f3ddbf6b51e0
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-01-24 08:56:48 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,15 @@
     <b-container>
       <b-row>
         <b-col style="padding: 5px" lg="4">
-          <sideProfile />
+          <sideProfile
+            class="animate__animated animate__fadeIn"
+            v-if="menuDisplay === 0"
+          />
+          <sideSetting v-else-if="menuDisplay === 1" />
+          <sideContact
+            class="animate__animated animate__zoomInUp"
+            v-else-if="menuDisplay === 2"
+          />
         </b-col>
         <b-col style="padding: 0" lg="7">
           <chatRoom />
@@ -16,11 +24,19 @@
 <script>
 import sideProfile from '../components/_base/home/sideProfile'
 import chatRoom from '../components/_base/home/chatRoom'
+import sideSetting from '../components/_base/home/settingSide'
+import sideContact from '../components/_base/home/sideContact'
+import { mapGetters } from 'vuex'
 export default {
   name: 'Home',
   components: {
     sideProfile,
-    chatRoom
+    chatRoom,
+    sideSetting,
+    sideContact
+  },
+  computed: {
+    ...mapGetters({ menuDisplay: 'menuDisplay' })
   }
 }
 </script>
